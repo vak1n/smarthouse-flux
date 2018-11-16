e1e5ec19e5bbe0ad2439679f99bbc6cdaf98b433
Ivan Kolobaev <ikolobai@yandex.ru>
2018-11-16 11:50:19 +0300

    Сборка

---
+++ dist/EventEmitter.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EventEmitter = (function () {
+    function EventEmitter() {
+        this.listeners = {};
+    }
+    EventEmitter.prototype.on = function (eventName, callback) {
+        if (!this.listeners[eventName]) {
+            this.listeners[eventName] = [];
+        }
+        this.listeners[eventName].push(callback);
+    };
+    EventEmitter.prototype.trigger = function (eventName, data) {
+        var listeners = this.listeners[eventName];
+        listeners.forEach(function (callback) {
+            callback(data);
+        });
+    };
+    return EventEmitter;
+}());
+exports.default = EventEmitter;
